766d099326317800b83a3de4197341aaac6bce3a
Torchi Rokaya <torchi.rokaya@innovatetech.co>
2025-03-19 18:10:06 +0545

    add env variables

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { config } from '../config'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDj2f9yKmIdjPHQ0USfm5CRB05DHttMHVw",
-  authDomain: "futsal-team-7a963.firebaseapp.com",
-  projectId: "futsal-team-7a963",
-  storageBucket: "futsal-team-7a963.firebasestorage.app",
-  messagingSenderId: "496159053019",
-  appId: "1:496159053019:web:c333c6b135b2a7761128fe",
-  measurementId: "G-4C288FS555"
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
+  projectId: config.projectId,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
+  appId: config.appId,
+  measurementId: config.measurementId
 };
 
 // Initialize Firebase
